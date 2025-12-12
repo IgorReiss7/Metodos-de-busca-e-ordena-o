00e22792c9188a55d4180c68f100917acd6bdcc8
lucaspachecopereira <lucaspachecopereira64@gmail.com>
2025-12-12 15:06:16 -0300

    Desenfileirar.c

--- src2/ExcluirFila.c
+++ src2/ExcluirFila.c
@@ -2,14 +2,16 @@
 #include "Animal.h"
 #include <string.h>
 #include "globals.h"
-void ExcluirFila(Animal filaAnimais[]){
-    if (qtd>0){
-        for(int i=0; i<qtd; i++){
+void Desenfileirar(){
+    if (qtdF>0){
+        pilhaAnimais[qtdP]=filaAnimais[0];
+        qtdP++;
+        for(int i=0; i<qtdF-1; i++){
             strcpy (filaAnimais[i].nome, filaAnimais[i+1].nome);
             strcpy (filaAnimais[i].especie, filaAnimais[i+1].especie);
             filaAnimais[i].idade = filaAnimais[i+1].idade;
         }
-        qtd--;
+        qtdF--;
     } else{
         printf("não há mais animais para serem excluidos");
     }

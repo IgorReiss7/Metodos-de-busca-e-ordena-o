c3381d8977129d43b74c9fe720236f08a78a0e83
lucaspachecopereira <lucaspachecopereira64@gmail.com>
2025-12-12 12:00:07 -0300

    ExcluirFila.c

--- src2/ExcluirFila.c
+++ src2/ExcluirFila.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include "Animal.h"
 #include <string.h>
-int qtd = 6;
-Animal filaAnimais[10] = {
-    {"totó","cachorro",14},
-    {"amora","gata",3},
-    {"mike","lagarto",5},
-    {"algodao","coelho",9},
-    {"donatello","tartaruga",8000},
-    {"filhodaputa","cavalo",2}
-};
-
-
+#include "globals.h"
 void ExcluirFila(Animal filaAnimais[]){
     if (qtd>0){
         for(int i=0; i<qtd; i++){
@@ -23,19 +13,4 @@ void ExcluirFila(Animal filaAnimais[]){
     } else{
         printf("não há mais animais para serem excluidos");
     }
-}
-int main (){
-    ExcluirFila(filaAnimais);
-    ExcluirFila(filaAnimais);
-    ExcluirFila(filaAnimais);
-    ExcluirFila(filaAnimais);
-    ExcluirFila(filaAnimais);
-    ExcluirFila(filaAnimais);
-    for (int i=0; i<qtd; i++){
-        printf("%s - %s - %d\n",
-           filaAnimais[i].nome,
-           filaAnimais[i].especie,
-           filaAnimais[i].idade);
-}
-    return 0;
 }
